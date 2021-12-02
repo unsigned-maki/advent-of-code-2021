4828f5bfee4d92a23766c889d9660582c89bc488
Niclas <nnell224@outlook.com>
2021-12-02 01:49:28 +0100

    Update day01.py
    Added solution for part 2

--- day01.py
+++ day01.py
@@ -1,6 +1,8 @@
 # https://adventofcode.com/2021/day/1
 report = open("input").readlines()
 
+# Part 1
+
 previous = 0
 increases = 0
 
@@ -9,5 +11,19 @@ for i in report:
         if int(i) > int(previous):
             increases = increases + 1
     previous = i
+
+print(increases)
+
+# Part 2
+
+previous = 0
+increases = 0
+
+for i in range(0, len(report)):
+    if i + 2 < len(report):
+        if previous:
+            if int(report[i]) + int(report[i+1]) + int(report[i+2])  > int(previous):
+                increases = increases + 1
+        previous = int(report[i]) + int(report[i+1]) + int(report[i+2])
 
 print(increases)
